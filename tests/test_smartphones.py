f13e7e8ba29a5f26918dca0a2134f878f2996b2c
Ivan Espinosa <ivangeg@icloud.com>
2026-01-13 14:01:58 -0600

    chore: remove ipytest dependencies and migrate to standard pytest

--- tests/test_smartphones.py
+++ tests/test_smartphones.py
@@ -2,12 +2,6 @@
 import pytest
 from src.smartphone_utils import prepare_smartphone_data
 
-import ipytest
-
-ipytest.config.rewrite_asserts = True
-__file__ = "notebook.ipynb"
-
-
 # Create a clean DataFrame fixture
 @pytest.fixture()
 def clean_smartphone_data():
@@ -22,6 +16,3 @@ def test_nan_values(clean_smartphone_data):
     # Assert there are no NaN value in "battery_capacity" or "os"
     assert clean_smartphone_data["battery_capacity"].isnull().sum() == 0
     assert not clean_smartphone_data["os"].isnull().sum() == 0
-
-    
-ipytest.run("-qq")
